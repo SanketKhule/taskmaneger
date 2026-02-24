dca7e046f410b99c195e0979c3f2be265dd50954
Sanket khule <sanketkhule5@gmail.com>
2026-02-24 08:57:34 +0530

    initial

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL)
+
+app.use("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Task Manager API" });
+})
+
+
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+        console.log(`mode ${process.env.NODE_ENV}`);
+        
+    })
+})
+
+module.exports = app;
